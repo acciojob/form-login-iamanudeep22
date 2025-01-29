7195c0e8de8decf39540686a3435da48baea7b63
Anudeep KN <116871481+iamanudeep22@users.noreply.github.com>
2025-01-30 00:12:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 function getFormvalue() {
     //Write your code here
-
+	const form1 = document.getElementById("form1");
+	const fName = form1.getElementsByTagName("input")[0].value;
+	const lName = form1.getElementsByTagName("input")[1].value;
+	alert(`${fName} ${lName}`);
 }
